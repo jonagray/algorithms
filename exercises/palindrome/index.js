8139bca0742bf98093968661a384dba07b1f82bb
jonagray <jon.a.graybill@gmail.com>
2022-03-01 16:29:04 -0800

    completed section 4

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,24 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Solution 1
+// function palindrome(str) {
+//   let reversed = '';
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//   if (reversed === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// Solution 2
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+} // What is happening here, is we are turning the string into an array, with each index being a character. Then, we use the .every() helper, which returns a boolean if each iteration of it returns true. Char argument is our starting value (beginning of the array), and "i" is our index. We are returning true for each character if it is equal to the string argument at str.length - i - 1, meaning we start at the end of the array, and decrement by 1 each time.
 
 module.exports = palindrome;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,9 +7,9 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 // Solution 1
-// function reverse(str) {
-//   return str.split('').reverse().join('');
-// }
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
 
 // Solution 2
 function reverse(str) {
@@ -23,11 +23,11 @@ function reverse(str) {
 }
 
 //Solution 3
-// function reverse(str) {
-//   return str.split('').reduce((reversed, character) => {
-//     return character + reversed;
-//   }, ''); // the purpose of reduce is to take an array and reduce it down to a single value | the second argument is a starting initial value for our function. The arrow function here takes that initial value (second argument), and then returns the result of the arrow function, and then uses that return value to be the starting value (second argument) of the next run of the arrow function. The arrow function runs one time for every element in the array.
-// }
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, ''); // the purpose of reduce is to take an array and reduce it down to a single value | the second argument is a starting initial value for our function. The arrow function here takes that initial value (second argument), and then returns the result of the arrow function, and then uses that return value to be the starting value (second argument) of the next run of the arrow function. The arrow function runs one time for every element in the array.
+}
 
 reverse('skeletons');
 
