149b296d0c6ea1260414b0bd296efb69c89c31a6
jonagray <jon.a.graybill@gmail.com>
2022-03-11 09:25:12 -0800

    completed section 6

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// Solution 1
+function maxChar(str) {
+  const chars = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    chars[char] = chars[char] + 1 || 1;
+  }
+  for (let char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
